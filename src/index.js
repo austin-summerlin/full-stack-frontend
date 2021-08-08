4e1791e445e2543891307dd6b180c3b17be063c7
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-08 16:52:31 -0700

    app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app/App';
+import Header from './components/app/Header';
+import Footer from './components/app/Footer';
 
 render(
-  <App />,
+  <Router>
+    <Header />
+    <App />
+    <Footer />
+  </Router>,
   document.getElementById('root')
 );
