8947f0f0860af213add627daec6aa3ee53378610
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-08 16:51:08 -0700

    footer

---
+++ src/components/app/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import style from './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className={style.footer}>
+        &copy; 2021 Ivy&#39;s House of Horror
+      </footer>
+    );
+  }
+}
+
+export default Footer;
+
+
