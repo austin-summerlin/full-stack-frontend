f613e9c9929af0ad537b57656621594ef6f140df
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-08 16:51:44 -0700

    movie

---
+++ src/components/Movie/MovieItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './MovieItem.css';
+
+const Movie = ({ title, genre, url }) => (
+  <figure className={style.movie}>
+    <h2>Name: {title}</h2>
+    <h3>Genre: {genre}</h3>
+    <img src={url} alt={title} />
+  </figure>
+);
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
+
+export default Movie;
