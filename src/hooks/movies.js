56a2fd2236de2cef4827fe2e973613a66c24cd9f
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-08 16:52:10 -0700

    movies

---
+++ src/hooks/movies.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { getMovies, getMovie } from '../services/movies-api';
+
+export const useMovies = (page) => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getMovies(page)
+      .then(setMovies)
+      .finally(() => setLoading(false));
+  }, [page]);
+
+  return { movies, loading };
+};
+
+export const useMovie = (id) => {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    getMovie(id)
+      .then(setMovie);
+  }, []);
+  return movie;
+};
+
